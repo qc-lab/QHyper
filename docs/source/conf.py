cf04388f185bd5c0271217b178ae411d6c4d7842
tmek1244 <tmek1244@wp.pl>
2022-11-26 13:59:36 +0100

    Docs fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../src'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 sys.path.insert(0, os.path.abspath('..'))
 
 # Configuration file for the Sphinx documentation builder.
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -4,7 +4,7 @@
    contain the root `toctree` directive.
 
 Welcome to QHyper's documentation!
-===============================
+==================================
 
 .. toctree::
    :maxdepth: 1
