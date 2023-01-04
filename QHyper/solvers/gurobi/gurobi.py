6df098ff48a937023360665b90a1d9d968c34ac2
jzawalska <justynajzawalska@gmail.com>
2023-01-04 16:30:53 +0100

    Update workflows. Add basic version of workflows generator

--- QHyper/solvers/gurobi/gurobi.py
+++ QHyper/solvers/gurobi/gurobi.py
@@ -1,8 +1,6 @@
 import gurobipy as gp
-import sympy
 
 from QHyper.problems.problem import Problem
-from QHyper.problems.workflow_scheduling import Workflow, WorkflowSchedulingProblem
 from QHyper.solvers.solver import Solver
 
 
@@ -13,25 +11,43 @@ class Gurobi(Solver):  # todo works only for quadratic expressions
     def solve(self):
         gpm = gp.Model("name")
 
-        # print("variables ", self.problem.variables, type(self.problem.variables), type(self.problem.variables[0]))
-        vars = {var_name: gpm.addVar(vtype=gp.GRB.BINARY, name=str(var_name)) for var_name in self.problem.variables}
-        gpm.update()
-        print(vars)
-        print(type(vars.keys()))
-
-        print((self.problem.objective_function.polynomial))
-        obj_fun = self.problem.objective_function.as_dict()
-        print("obj_fun ", obj_fun)
-
-        cost_function = 0
-        for key, value in obj_fun.items():  # todo
-            print(key, value)
-            tmp = 1
-            for k in key:
-                tmp *= vars[k]
-            cost_function += tmp * value
-        #     print(key, value)
-
+        vars = {str(var_name): gpm.addVar(vtype=gp.GRB.BINARY, name=str(var_name))
+                for var_name in self.problem.variables}
         # gpm.update()
-        # print(gpm)
+
+        objective_function = calc(vars, self.problem.objective_function.as_dict())
+        gpm.setObjective(objective_function, gp.GRB.MINIMIZE)
+
+        eq_constraints = self.problem.constraints["=="]
+        for i, constraint in enumerate(eq_constraints):
+            tmp_constraint = calc(vars, constraint.as_dict())
+            gpm.addConstr(tmp_constraint == 0, f"eq_constr_{i}")
+
+        ltq_constraints = self.problem.constraints["<="]
+        for i, constraint in enumerate(ltq_constraints):
+            tmp_constraint = calc(vars, constraint.as_dict())
+            gpm.addConstr(tmp_constraint <= 0, f"leq_constr_{i}")
+
+        gtq_constraints = self.problem.constraints["<="]
+        for i, constraint in enumerate(gtq_constraints):
+            tmp_constraint = calc(vars, constraint.as_dict())
+            gpm.addConstr(tmp_constraint >= 0, f"gtq_constr_{i}")
+
         gpm.optimize()
+
+        allvars = gpm.getVars()
+        solution = {}
+        for v in allvars:
+            solution[v.VarName] = v.X
+
+        return solution
+
+
+def calc(vars, poly_dict):
+    cost_function = 0
+    for key, value in poly_dict.items():
+        tmp = 1
+        for k in key:
+            tmp *= vars[k]
+        cost_function += tmp * value
+    return cost_function
