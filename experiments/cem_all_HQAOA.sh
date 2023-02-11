f9f9440dea286bd20fcd1f1493e0183fc4abd9bb
Tomasz Lamza <tmek1244@wp.pl>
2023-02-11 11:33:14 +0100

    update cem experiments

--- experiments/cem_all_HQAOA.sh
+++ experiments/cem_all_HQAOA.sh
@@ -3,11 +3,11 @@
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=32
 #SBATCH -A plgkwantowy2-cpu
-#SBATCH -o results_4/cem_all.txt
+#SBATCH -o results_4/cem_all_HQAOA.txt
 # the above lines are read and interpreted by sbatch, this one and leter are not
 # command, this will be executed on a compute node
 
 source ../venv/bin/active
 module load python/3.10.4-gcccore-11.3.0
 
-python3 cem_all.py
+python3 cem_all_HQAOA.py
