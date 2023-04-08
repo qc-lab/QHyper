78a0a23a4b59d828b2c3a67b42f781d0ec650a2b
tmek1244 <tmek1244@wp.pl>
2023-04-08 19:27:15 +0200

    Add multiprocessing support

--- paper_v2/test.py
+++ paper_v2/test.py
@@ -1,4 +1,5 @@
 import argparse
+import multiprocessing as mp
 
 import numpy as np
 
@@ -47,12 +48,43 @@ TESTER_CONFIG = {
     }
 }
 
+class Wrapper:
+    def __init__(self, max_weight, items, samples, output_file) -> None:
+        knapsack = KnapsackProblem(
+            max_weight=max_weight, items=items)
+         
+        self.qaoa = VQA(knapsack, config=QAOA_CONFIG)
+        self.wfqaoa = VQA(knapsack, config=WFQAOA_CONFIG)
+        self.hqaoa = VQA(knapsack, config=HQAOA_CONFIG)
+        self.tester = VQA(knapsack, config=TESTER_CONFIG)
+
+        self.angles = 2 * np.pi * np.random.rand(samples, 2, 5)
+        self.weights = 10*np.random.rand(samples, 2) + 1
+        self.output_file = output_file
+
+        with open(output_file, mode='a') as output:
+            print(f"{knapsack.knapsack.items} {knapsack.knapsack.max_weight}", file=output)
+            print("QAOA;WFQAOA;HQAOA", file=output)
+
+    def __call__(self, i) -> None:
+        with open(self.output_file, mode='a') as output:
+            params_cofing = {
+                'angles': self.angles[i],
+                'hyper_args': list(self.weights[i]) + [self.weights[i][1]]
+            }
+            for solver in [self.qaoa, self.wfqaoa, self.hqaoa]:
+                best_params = solver.solve(params_cofing)
+
+                print(f"{self.tester.evaluate(best_params):.3f}", end=';', file=output)
+            print(file=output)
+
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument('--samples', default=20, type=int)
     parser.add_argument('-w', type=int)
     parser.add_argument('--weights', metavar='N', type=int, nargs='+')
     parser.add_argument('--values', metavar='N', type=int, nargs='+')
+    parser.add_argument('--processes', type=int, default=1)
 
     return parser.parse_args()
 
@@ -62,28 +94,14 @@ if __name__ == '__main__':
 
     if len(args.weights) != len(args.values):
         raise Exception("Number of provided weights and values must be the same")
-    knapsack = KnapsackProblem(max_weight=args.w, items=list(zip(args.weights, args.values)))
-   
-    qaoa = VQA(knapsack, config=QAOA_CONFIG)
-    wfqaoa = VQA(knapsack, config=WFQAOA_CONFIG)
-    hqaoa = VQA(knapsack, config=HQAOA_CONFIG)
-    tester = VQA(knapsack, config=TESTER_CONFIG)
-
+    items=list(zip(args.weights, args.values))
     output_file = f'{args.w}_{"_".join([f"{w}-{v}" for w,v in list(zip(args.weights, args.values))])}.csv'
-
-    with open(output_file, mode='w') as output:
-        print(knapsack.knapsack.items, file=output)
-        print("QAOA;WFQAOA;HQAOA", file=output)
-
-        angles = 2 * np.pi * np.random.rand(args.samples, 2, 5)
-        weights = 10*np.random.rand(args.samples, 2) + 1
-        for i in range(args.samples):
-            params_cofing = {
-                'angles': angles[i],
-                'hyper_args': list(weights[i]) + [weights[i][1]]
-            }
-            for solver in [qaoa, wfqaoa, hqaoa]:
-                best_params = solver.solve(params_cofing)
-
-                print(f"{tester.evaluate(best_params):.3f}", end=';', file=output)
-            print(file=output)
+    wrapper = Wrapper(args.w, items, args.samples, output_file)
+   
+    # qaoa = VQA(knapsack, config=QAOA_CONFIG)
+    # wfqaoa = VQA(knapsack, config=WFQAOA_CONFIG)
+    # hqaoa = VQA(knapsack, config=HQAOA_CONFIG)
+    # tester = VQA(knapsack, config=TESTER_CONFIG)
+        
+    with mp.Pool(processes=args.processes) as pool:
+        pool.map(wrapper, range(args.samples))
