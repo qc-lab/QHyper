35030cf13e2c35024b718d359c440b17f981b529
Tomasz Lamza <tmek1244@wp.pl>
2023-04-16 12:56:15 +0200

    More basinhopping results

--- paper_bh/bh_10.sh
+++ paper_bh/bh_10.sh
@@ -10,4 +10,4 @@ module load python/3.10.4-gcccore-11.3.0
 # source /net/people/plgrid/plgtmek1244/QHyper/venv/bin/active
 
 # python3 experiments.py
-python3 test_basinhopping.py -w 2 --values 2 2 1 --weights 1 1 1 --samples 10 --processes 10 --niter 10
+python3 test_basinhopping.py -w 2 --values 2 2 1 --weights 1 1 1 --samples 1000 --processes 10 --niter 10
--- paper_bh/bh_50.sh
+++ paper_bh/bh_50.sh
@@ -10,4 +10,4 @@ module load python/3.10.4-gcccore-11.3.0
 # source /net/people/plgrid/plgtmek1244/QHyper/venv/bin/active
 
 # python3 experiments.py
-python3 test_basinhopping.py -w 2 --values 2 2 1 --weights 1 1 1 --samples 10 --processes 10 --niter 50
+python3 test_basinhopping.py -w 2 --values 2 2 1 --weights 1 1 1 --samples 200 --processes 10 --niter 50
