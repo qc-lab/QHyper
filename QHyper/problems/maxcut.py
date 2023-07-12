21412d066c069e8090caa3ba2b06110d24eb43c6
basiav <82549088+basiav@users.noreply.github.com>
2023-07-12 10:47:26 +0200

    Added and fixed Tomek's maxcut version

--- QHyper/problems/maxcut.py
+++ QHyper/problems/maxcut.py
@@ -1,6 +1,4 @@
-import random
 import sympy
-from collections import namedtuple
 
 from typing import cast
 
@@ -11,17 +9,25 @@ from QHyper.hyperparameter_gen.parser import Expression
 class MaxCutProblem(Problem):
     def __init__(self, edges: list[tuple[int, int]]) -> None:
         self.edges = edges
-        self.variables = sympy.symbols(' '.join(
-            [f'x{i}' for i in range(max(v for edge in edges for v in edge) + 1)]
-        ))
+        self.variables = sympy.symbols(
+            " ".join(
+                [
+                    f"x{i}"
+                    for i in range(
+                        len(list(set([v for edge in edges for v in edge])))
+                    )
+                ]
+            )
+        )
 
         self._set_objective_function()
+        self.constraints = []
 
     def _set_objective_function(self) -> None:
         equation = cast(sympy.Expr, 0)
 
         for e in self.edges:
-            x_i, x_j = self.variables[e[0]], self.variables[e[1]]
+            x_i, x_j = self.variables[e[0] - 1], self.variables[e[1] - 1]
             equation -= x_i * (1 - x_j) + x_j * (1 - x_i)
 
         self.objective_function: Expression = Expression(equation)
