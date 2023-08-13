1f95b700fbc56b72befdd361b5833a37c31016af
tmek1244 <tmek1244@wp.pl>
2023-08-13 12:45:12 +0200

    Add setup file and github actions to automatically push new release on tags

---
+++ setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+import subprocess
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+def get_version():
+    try:
+        tag = subprocess.check_output(["git", "describe", "--tags"]).strip().decode("utf-8")
+        return tag
+    except Exception as e:
+        print("Error:", e)
+        return "0.1.0"  # Default version if Git tag extraction fails
+
+
+DESCRIPTION = 'Quantum and classical problem solvers'
+LONG_DESCRIPTION = 'A package that allows to build and solve quantum and classical problems using predefined solvers and problems.'
+
+
+# Setting up
+setup(
+    name="qhyper",
+    version=get_version(),
+    author="ACK Cyfronet AGH",
+    author_email="tomasz.lamza@cyfronet.pl",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'PennyLane', 'tdqm', 'sympy', 'dwave-system', 'gurobipy', 'pandas', 'wfcommons'],
+    keywords=['python', 'qhyper', 'quantum', 'solver', 'experiment'],
+    license='MIT',
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
