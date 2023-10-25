1054cff1caf14630087eadd1116db7991b72007f
Katarzyna Rycerz <kzajac@agh.edu.pl>
2023-10-25 21:11:42 +0200

    test

---
+++ demo/testQNG.py
@@ -0,0 +1,23 @@
+import pennylane as qml
+from pennylane import numpy as np
+
+dev = qml.device("lightning.qubit", wires=1)
+
+
+@qml.qnode(dev, interface="autograd")
+def circuit(params):
+    # |psi_0>: state preparation
+    qml.Hadamard(wires=0)
+   # qml.RY(np.pi/2, wires=0)
+
+    # V0(theta0, theta1): Parametrized layer 0
+    qml.RZ(params[0],wires=0)
+    qml.RX(params[1],wires=0)
+
+   
+   
+    return qml.expval(qml.PauliZ(0))
+
+
+params = np.array([0.1e-13, 0.1e-13])
+print(qml.metric_tensor(circuit, approx='diag')(params))
