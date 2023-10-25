1acb1db1d73c866a072d98cda55fea20f1866360
Katarzyna Rycerz <kzajac@agh.edu.pl>
2023-10-25 11:48:22 +0200

    test

--- QHyper/solvers/vqa/pqc/sqaoa.py
+++ QHyper/solvers/vqa/pqc/sqaoa.py
@@ -162,7 +162,7 @@ class SQAOA(PQC):
            return qml.expval(
                cost_operator)
         
-        opt = qml.QNGOptimizer(0.001)
+        opt = qml.QNGOptimizer(0.0001)
         params = np.array(opt_args, requires_grad=True)
         for ind in range(50):
             params, cost = opt.step_and_cost(expval_circuit,params)
