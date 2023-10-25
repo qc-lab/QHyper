2b665fe3661a3fbc51e81326d9a2e19c016e5232
Katarzyna Rycerz <kzajac@agh.edu.pl>
2023-10-25 16:36:33 +0200

    test

--- demo/testyworkflow.py
+++ demo/testyworkflow.py
@@ -153,12 +153,12 @@ for constraint in problem.constraints:
     print(f"    {constraint}")
     
 params_cofing = {
-        'angles': [[-1.46546167e-03, -1.47630043e-03,  5.23969216e-04,
-          4.32957649e-04,  7.74771138e-04],
-        [-5.95883641e+01, -8.44669790e-01, -8.87181353e-01,
-         -5.11625786e-01, -5.02607625e-01]],
+      #  'angles': [[-1.46546167e-03, -1.47630043e-03,  5.23969216e-04,
+     #    4.32957649e-04,  7.74771138e-04],
+      #  [-5.95883641e+01, -8.44669790e-01, -8.87181353e-01,
+      #   -5.11625786e-01, -5.02607625e-01]],
 
-       # 'angles': [[0.1e-13]*5, [0.1e-13]*5], # QAOA angles - first we have gammas (for the cost Hamiltonian), then we have betas (for the mixer)
+        'angles': [[0.1e-13]*5, [0.1e-13]*5], # QAOA angles - first we have gammas (for the cost Hamiltonian), then we have betas (for the mixer)
         'hyper_args': [1, # do not change - this should be the weight for the 'cost function' but since in our cost function 
                           # we also have the deadline in the linear form (as of now it needs to be implemented this way due to QHyper limitations)
                           # the weight for the actual cost function is set there. THIS WILL NOT WORK WELL WITH HYPER-QAOA.
