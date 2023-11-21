9631a7b98ec81a5457dbfc1a5feab336511d1a16
Katarzyna Rycerz <kzajac@agh.edu.pl>
2023-11-21 17:29:10 +0100

    test

--- QHyper/solvers/vqa/pqc/obrobka_danych.py
+++ QHyper/solvers/vqa/pqc/obrobka_danych.py
@@ -1,12 +1,13 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-
-steps=50
+res=pd.read_csv(".\demo\wyniki\probability_step0.csv").sort_values('energy')
+print(res.energy)
+steps=0
 for i in range(steps):
     
     res=pd.read_csv("probability_step"+str(i)+".csv").sort_values('result')
-    
+    print(res.results)
     res.plot(x='result', y='prop', kind='bar',ylim=(0, 0.05))
     
     plt.savefig("probability_stepr"+str(i)+".png")
--- demo/wfbinhist.py
+++ demo/wfbinhist.py
@@ -25,127 +25,128 @@ deadline = 13
 
 class SimpleWorkflowProblem(Problem):
 
-
     def __init__(self) -> None:
-        #uwaga: to jest na sztywno
-        num_of_qubits = 6 #wsp.workflow.cost_matrix.shape[0] * wsp.workflow.cost_matrix.shape[1]
-        self.variables = sympy.symbols(' '.join([f'x{i}' for i in range(num_of_qubits)]))
-        self._set_objective_function()
-        self._set_constraints()
-
+            #uwaga: to jest na sztywno
+            num_of_qubits = 6 #wsp.workflow.cost_matrix.shape[0] * wsp.workflow.cost_matrix.shape[1]
+            self.variables = sympy.symbols(' '.join([f'x{i}' for i in range(num_of_qubits)]))                                  
+            self._set_objective_function()
+            self._set_constraints()
+            
     def _set_objective_function(self) -> None:
+#zamiana M00 z M10    
+#zamiana T2 z T3        
+            C_f = C_f = (8.0*(1-self.variables[0])*(1-self.variables[1]) 
+        + 8.0*(1-self.variables[0])*(self.variables[1])  
+        + 6.0*(self.variables[0])*(1-self.variables[1]) 
+        + 2.0*(self.variables[0])*(self.variables[1])  
+        + 4.0*(1-self.variables[4])*(1-self.variables[5]) 
+        + 4.0*(1-self.variables[4])*(self.variables[5])  
+        + 3.0*(self.variables[4])*(1-self.variables[5]) 
+        + 1.0*(self.variables[4])*(self.variables[5])
+        + 16.0*(1-self.variables[2])*(1-self.variables[3]) 
+        + 16.0*(1-self.variables[2])*(self.variables[3])  
+        + 12.0*(self.variables[2])*(1-self.variables[3]) 
+        + 4.0*(self.variables[2])*(self.variables[3])  )
+            
+            self.deadline_aux=(4.0*(1-self.variables[0])*(1-self.variables[1]) 
+        + 2.0*(1-self.variables[0])*(self.variables[1])  
+            + 6.0*(self.variables[0])*(1-self.variables[1]) 
+            + 16.0*(self.variables[0])*(self.variables[1])  
+            + 2.0*(1-self.variables[4])*(1-self.variables[5]) 
+            + 1.0*(1-self.variables[4])*(self.variables[5])  
+            + 3.0*(self.variables[4])*(1-self.variables[5]) 
+            + 8.0*(self.variables[4])*(self.variables[5])
+            + 8.0*(1-self.variables[2])*(1-self.variables[3]) 
+            + 4.0*(1-self.variables[2])*(self.variables[3])  
+            + 12.0*(self.variables[2])*(1-self.variables[3]) 
+            + 32.0*(self.variables[2])*(self.variables[3])  )
 
-        C_f = (6.0*(1-self.variables[0])*(1-self.variables[1])
-        + 8.0*(1-self.variables[0])*(self.variables[1])
-        + 8.0*(self.variables[0])*(1-self.variables[1])
-        + 2.0*(self.variables[0])*(self.variables[1])
-        + 3.0*(1-self.variables[2])*(1-self.variables[3])
-        + 4.0*(1-self.variables[2])*(self.variables[3])
-        + 4.0*(self.variables[2])*(1-self.variables[3])
-        + 1.0*(self.variables[2])*(self.variables[3])
-        + 12.0*(1-self.variables[4])*(1-self.variables[5])
-        + 16.0*(1-self.variables[4])*(self.variables[5])
-        + 16.0*(self.variables[4])*(1-self.variables[5])
-        + 4.0*(self.variables[4])*(self.variables[5])  )
+            K_f4_linear = deadline - self.deadline_aux
+            K_f4_squared=K_f4_linear**2
+            K_f4_squared1=sympy.expand(K_f4_squared)
 
-        self.deadline_aux=(6.0*(1-self.variables[0])*(1-self.variables[1])
-       + 2.0*(1-self.variables[0])*(self.variables[1])
-        + 4.0*(self.variables[0])*(1-self.variables[1])
-        + 16.0*(self.variables[0])*(self.variables[1])
-        + 3.0*(1-self.variables[2])*(1-self.variables[3])
-        + 1.0*(1-self.variables[2])*(self.variables[3])
-        + 2.0*(self.variables[2])*(1-self.variables[3])
-        + 8.0*(self.variables[2])*(self.variables[3])
-        + 12.0*(1-self.variables[4])*(1-self.variables[5])
-        + 4.0*(1-self.variables[4])*(self.variables[5])
-        + 8.0*(self.variables[4])*(1-self.variables[5])
-        + 32.0*(self.variables[4])*(self.variables[5])  )
-
-        K_f4_linear = deadline - self.deadline_aux
-        K_f4_squared=K_f4_linear**2
-        K_f4_squared1=sympy.expand(K_f4_squared)
-
-       # print(K_f4_squared1, "\n\n")
-
-        for i in range(6):
-            K_f4_squared1=K_f4_squared1.subs(self.variables[i]**2, self.variables[i])
-        #print(K_f4_squared1,"\n\n")
-
-       # print(K_f4_squared1)
-        self.objective_function = (Expression(hyper_params['cost_function_weight'] * C_f
-                                              + hyper_params['deadline_linear_form_weight'] *  K_f4_linear
-                                   + hyper_params['deadline_quadratic_form_weight'] * K_f4_linear**2))
-
+        # print(K_f4_squared1, "\n\n")
+            
+            for i in range(6):
+                K_f4_squared1=K_f4_squared1.subs(self.variables[i]**2, self.variables[i])
+            #print(K_f4_squared1,"\n\n")  
+            
+        # print(K_f4_squared1)     
+            self.objective_function = (Expression(hyper_params['cost_function_weight'] * C_f 
+                                                + hyper_params['deadline_linear_form_weight'] *  K_f4_linear
+                                    + hyper_params['deadline_quadratic_form_weight'] * K_f4_linear**2))
+            
     def _set_constraints(self):
-       # print(self.deadline_aux)
+        # print(self.deadline_aux)
 
-
-
-
-        self.constraints = []
-
+            
+        
+                
+            self.constraints = []
+        
     def get_score(self, result, penalty=0):
-     
-        x = [int(val) for val in result]
-         
-        if (6.0*(1.0-x[0])*(1-x[1])
-        + 2.0*(1.0-x[0])*(x[1])
-        + 4.0*(x[0])*(1.0-x[1])
-        + 16.0*(x[0])*(x[1])
-        + 3.0*(1.0-x[2])*(1.0-x[3])
-        + 1.0*(1.0-x[2])*(x[3])
-        + 2.0*(x[2])*(1.0-x[3])
-        + 8.0*(x[2])*(x[3])
-        + 12.0*(1.0-x[4])*(1.0-x[5])
-        + 4.0*(1.0-x[4])*(x[5])
-        + 8.0*(x[4])*(1.0-x[5])
-        + 32.0*(x[4])*(x[5]) <=13 ):
-            return (6.0*(1.0-x[0])*(1.0-x[1])
-        + 8.0*(1.0-x[0])*(x[1])
-        + 8.0*(x[0])*(1.0-x[1])
-        + 2.0*(x[0])*(x[1])
-        + 3.0*(1.0-x[2])*(1.0-x[3])
-        + 4.0*(1.0-x[2])*(x[3])
-        + 4.0*(x[2])*(1.0-x[3])
-        + 1.0*(x[2])*(x[3])
-        + 12.0*(1.0-x[4])*(1.0-x[5])
-        + 16.0*(1.0-x[4])*(x[5])
-        + 16.0*(x[4])*(1.0-x[5])
-        + 4.0*(x[4])*(x[5]) )
-
-        return penalty
+            
+            x = [int(val) for val in result]
+        
+            if (4.0*(1.0-x[0])*(1-x[1]) 
+            + 2.0*(1.0-x[0])*(x[1])  
+            + 6.0*(x[0])*(1.0-x[1]) 
+            + 16.0*(x[0])*(x[1])  
+            + 2.0*(1.0-x[4])*(1.0-x[5]) 
+            + 1.0*(1.0-x[4])*(x[5])  
+            + 3.0*(x[4])*(1.0-x[5]) 
+            + 8.0*(x[4])*(x[5])
+            + 8.0*(1.0-x[2])*(1.0-x[3]) 
+            + 4.0*(1.0-x[2])*(x[3])  
+            + 12.0*(x[2])*(1.0-x[3]) 
+            + 32.0*(x[2])*(x[3]) <=13 ):
+                
+                return (8.0*(1.0-x[0])*(1.0-x[1]) 
+            + 8.0*(1.0-x[0])*(x[1])  
+            + 6.0*(x[0])*(1.0-x[1]) 
+            + 2.0*(x[0])*(x[1])  
+            + 4.0*(1.0-x[4])*(1.0-x[5]) 
+            + 4.0*(1.0-x[4])*(x[5])  
+            + 3.0*(x[4])*(1.0-x[5]) 
+            + 1.0*(x[4])*(x[5])
+            + 16.0*(1.0-x[2])*(1.0-x[3]) 
+            + 16.0*(1.0-x[2])*(x[3])  
+            + 12.0*(x[2])*(1.0-x[3]) 
+            + 4.0*(x[2])*(x[3]) ) 
+            
+            return penalty
     
     def get_qaoa_energy(self, result):
 
         x = [int(val) for val in result]
      
-        tcost=(6.0*(1.0-x[0])*(1.0-x[1]) 
-        + 8.0*(1.0-x[0])*(x[1])
-        + 8.0*(x[0])*(1.0-x[1]) 
-        + 2.0*(x[0])*(x[1]) 
-        + 3.0*(1.0-x[2])*(1.0-x[3]) 
-        + 4.0*(1.0-x[2])*(x[3])
-        + 4.0*(x[2])*(1.0-x[3])
-        + 1.0*(x[2])*(x[3])
-        + 12.0*(1.0-x[4])*(1.0-x[5])
-        + 16.0*(1.0-x[4])*(x[5])
-        + 16.0*(x[4])*(1.0-x[5])
-        + 4.0*(x[4])*(x[5])) 
+        tcost=(8.0*(1.0-x[0])*(1.0-x[1]) 
+            + 8.0*(1.0-x[0])*(x[1])  
+            + 6.0*(x[0])*(1.0-x[1]) 
+            + 2.0*(x[0])*(x[1])  
+            + 4.0*(1.0-x[4])*(1.0-x[5]) 
+            + 4.0*(1.0-x[4])*(x[5])  
+            + 3.0*(x[4])*(1.0-x[5]) 
+            + 1.0*(x[4])*(x[5])
+            + 16.0*(1.0-x[2])*(1.0-x[3]) 
+            + 16.0*(1.0-x[2])*(x[3])  
+            + 12.0*(x[2])*(1.0-x[3]) 
+            + 4.0*(x[2])*(x[3]) ) 
         
    
         
-        linear=(deadline-(6.0*(1.0-x[0])*(1-x[1])
-        + 2.0*(1.0-x[0])*(x[1])
-        + 4.0*(x[0])*(1.0-x[1])
-        + 16.0*(x[0])*(x[1])
-        + 3.0*(1.0-x[2])*(1.0-x[3])
-        + 1.0*(1.0-x[2])*(x[3])
-        + 2.0*(x[2])*(1.0-x[3])
-        + 8.0*(x[2])*(x[3])
-        + 12.0*(1.0-x[4])*(1.0-x[5])
-        + 4.0*(1.0-x[4])*(x[5])
-        + 8.0*(x[4])*(1.0-x[5])
-        + 32.0*(x[4])*(x[5])))
+        linear=(deadline-(4.0*(1.0-x[0])*(1-x[1]) 
+            + 2.0*(1.0-x[0])*(x[1])  
+            + 6.0*(x[0])*(1.0-x[1]) 
+            + 16.0*(x[0])*(x[1])  
+            + 2.0*(1.0-x[4])*(1.0-x[5]) 
+            + 1.0*(1.0-x[4])*(x[5])  
+            + 3.0*(x[4])*(1.0-x[5]) 
+            + 8.0*(x[4])*(x[5])
+            + 8.0*(1.0-x[2])*(1.0-x[3]) 
+            + 4.0*(1.0-x[2])*(x[3])  
+            + 12.0*(x[2])*(1.0-x[3]) 
+            + 32.0*(x[2])*(x[3])))
       
         return (hyper_params['cost_function_weight'] * tcost
                                               + hyper_params['deadline_linear_form_weight'] *  linear
@@ -162,17 +163,14 @@ print(f"Objective function: {problem.objective_function}")
 print("Constraints (RHS == 0):")
 for constraint in problem.constraints:
     print(f"    {constraint}")
-    
+##ang= (2*np.pi)/(3791-25)
+##print(ang)
+ang=0.1e-13
+layers=7
 params_config = {
        #'angles': [[0.1e-13,0.1,0.1e-13,0.1e-13,0.1e-13,0.1e13], [np.pi/2]*6], # QAOA angles - first we have gammas (for the cost Hamiltonian), then we have betas (for the mixer)
-   'angles':[[8.66892857,  1.00000145e-01, -1.91456774e-06, -1.08992071e-07,
-  -1.83911351e-06,  1.00000000e+12],
- [-4.93647311e+01,  1.57310046e+00,  1.57309622e+00,  1.57310244e+00,
-   1.57309841e+00,  1.58401274e+00]],
-    #'angles': [[3.23810485e-04,  3.89068182e-04,  4.08362541e-04,  2.18136406e-04,
-   #3.91692476e-04,  3.01205096e-04],
- #[-2.51645530e+02, -1.22816763e+02, -1.20555243e+02, -9.45352537e+01,
-  #-9.88528753e+01, -8.19648493e+01]] , 
+   'angles':[[ang]*layers, [np.pi/2]*layers],
+    
     'hyper_args': [1, # do not change - this should be the weight for the 'cost function' but since in our cost function
                           # we also have the deadline in the linear form (as of now it needs to be implemented this way due to QHyper limitations)
                           # the weight for the actual cost function is set there. THIS WILL NOT WORK WELL WITH HYPER-QAOA.
@@ -181,14 +179,14 @@ params_config = {
 }
 
 from QHyper.solvers import VQA
-steps=3
+steps=10
 solver_config = {
     "pqc": {
         "type": "qml_qaoa",
-        "layers": 6,
+        "layers": layers,
         "optimizer": "qng",
         "optimizer_args": {
-            "stepsize": 0.00001,
+            "stepsize": 0.0001,
             "steps": steps,
             "verbose": True,
         },
@@ -201,18 +199,39 @@ vqa = VQA.from_config(problem, config=solver_config)
 
 solver_results = vqa.solve()
 
+from QHyper.util import (
+    weighted_avg_evaluation, sort_solver_results, add_evaluation_to_results)
+
+print(f"Best params: {solver_results.params}")
+# Evaluate results with weighted average evaluation
+print("Evaluation:")
+print(weighted_avg_evaluation(
+    solver_results.results_probabilities, problem.get_score,
+    penalty=0, limit_results=64, normalize=True
+))
+print("Sort results:")
+sorted_results1 = sort_solver_results(
+    solver_results.results_probabilities, limit_results=64)
+
+# Add evaluation to results
+results_with_evaluation = add_evaluation_to_results(
+    sorted_results1, problem.get_score, penalty=0)
+
+for result, (probability, evaluation) in results_with_evaluation.items():
+    print(f"Result: {result}, "
+          f"Prob: {probability:.5}, "
+          f"Evaluation: {evaluation}")
 
 tester_config = {
     'pqc': {
         'type': 'qaoa',
-        'layers': 6,
+        'layers': layers,
     }
 }
 tester = VQA.from_config(problem, config=tester_config)
 import pandas as pd
 import matplotlib.pyplot as plt
-from QHyper.util import (
-    weighted_avg_evaluation, sort_solver_results, add_evaluation_to_results)
+
 
 for i in range(steps):
     print(i+1,solver_results.history[0][i].params)
@@ -228,12 +247,12 @@ for i in range(steps):
  
     res=pd.DataFrame(data={'result': tester_results.results_probabilities.keys(),
                        'prop': tester_results.results_probabilities.values(),'energy':en}).sort_values('energy')
-    res.to_csv("probability_step"+str(i+1)+".csv")
+    res.to_csv(".\demo\wynikirob\probability_step"+str(i+1)+".csv")
     
     res.plot(x='energy', y='prop', kind='bar',ylim=(0, 0.05))
     
 # Import matplotlib
-    plt.savefig("probability_step"+str(i+1)+".png")
+    plt.savefig(".\demo\wynikirob\probability_step"+str(i+1)+".png")
     plt.close()
 
 
