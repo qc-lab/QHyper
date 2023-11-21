f4ecebbbeb974cb6f3c5597ff81bc7ceddbd5418
Katarzyna Rycerz <kzajac@agh.edu.pl>
2023-11-21 15:11:07 +0100

    test

--- QHyper/solvers/vqa/pqc/obrobka_danych.py
+++ QHyper/solvers/vqa/pqc/obrobka_danych.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 steps=50
 for i in range(steps):
     
-    res=pd.read_csv("probability_step"+str(i)+".csv")
+    res=pd.read_csv("probability_step"+str(i)+".csv").sort_values('result')
     
-    res.plot(x='energy', y='prop', kind='bar',ylim=(0, 0.05))
+    res.plot(x='result', y='prop', kind='bar',ylim=(0, 0.05))
     
-    plt.savefig("probability_step"+str(i)+".png")
+    plt.savefig("probability_stepr"+str(i)+".png")
     plt.close()
