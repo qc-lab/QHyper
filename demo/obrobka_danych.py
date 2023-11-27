53c53494ebd5813bf3941e0f154ee3f0651f3b68
Katarzyna Rycerz <kzajac@agh.edu.pl>
2023-11-27 16:45:04 +0100

    test

--- demo/obrobka_danych.py
+++ demo/obrobka_danych.py
@@ -1,14 +1,15 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-res=pd.read_csv(".\demo\wyniki\probability_step0.csv").sort_values('energy')
-print(res.energy)
-steps=0
+#res=pd.read_csv(".\demo\wynikirob\probability_step0.csv").sort_values('energy')
+#print(res.energy)
+steps=30
 for i in range(steps):
     
-    res=pd.read_csv("probability_step"+str(i)+".csv").sort_values('result')
-    print(res.results)
+    res=pd.read_csv(".\demo\wynikirob\probability_step"+str(i+1)+".csv")
+    #.sort_values('result')
+
     res.plot(x='result', y='prop', kind='bar',ylim=(0, 0.3))
     
-    plt.savefig("probability_stepr"+str(i)+".png")
+    plt.savefig(".\demo\wynikirob\probability_stepr"+str(i+1)+".png")
     plt.close()
