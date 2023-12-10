0677f8f9bb53117804e8136238c7b841a29456ac
tmek1244 <tmek1244@wp.pl>
2023-12-10 15:27:49 +0100

    Add mention of EuroHPC PL

--- QHyper/problems/workflow_scheduling.py
+++ QHyper/problems/workflow_scheduling.py
@@ -1,3 +1,8 @@
+# This work was supported by the EuroHPC PL infrastructure funded at the
+# Smart Growth Operational Programme (2014-2020), Measure 4.2
+# under the grant agreement no. POIR.04.02.00-00-D014/20-00
+
+
 import math
 from collections import defaultdict
 from dataclasses import dataclass
