c4ae84857e9b33596b6f5f0931157efe03544af5
Kacper <kacper3615@gmail.com>
2024-03-06 14:58:35 +0100

    Applied changes to modularity calculation and objective function

--- QHyper/problems/community_detection.py
+++ QHyper/problems/community_detection.py
@@ -16,8 +16,11 @@ class Network:
     resolution: float = field(default=1)
     modularity_matrix: np.ndarray = field(init=False)
     weight: str | None = field(default=None)
+    community: list | None= field(default=None)
 
     def __post_init__(self) -> None:
+        if not self.community:
+            self.community = [*range(self.graph.number_of_nodes())]
         self.modularity_matrix = self.calculate_modularity_matrix()
 
     def calculate_modularity_matrix(self) -> np.ndarray:
@@ -26,11 +29,19 @@ class Network:
         )
         degree_matrix: np.ndarray = adj_matrix.sum(axis=1)
         m: int = np.sum(degree_matrix)
-        return (
+
+        full_modularity_matrix = (
             adj_matrix
             - self.resolution * np.outer(degree_matrix, degree_matrix) / m
         )
 
+        B_bis = full_modularity_matrix[self.community,:]
+        B_community = B_bis[:,self.community]
+        B_i = np.sum(B_community, axis=-1)
+        delta = np.eye(len(self.community), dtype=np.int32)
+        B_g = B_community - delta*B_i
+        return B_g
+    
 
 class KarateClubNetwork(Network):
     def __init__(self, resolution: float = 1):
@@ -96,7 +107,7 @@ class CommunityDetectionProblem(Problem):
             raise Exception(
                 "Number of communities must be greater than or equal to 1"
             )
-
+        self.community = network_data.community
         self.cases: int = communities
         self.one_hot_encoding: bool = one_hot_encoding
         self.resolution: float = network_data.resolution
@@ -135,27 +146,38 @@ class CommunityDetectionProblem(Problem):
     def _set_objective_function(self) -> None:
         equation: dict[VARIABLES, float] = {}
         _, degrees = zip(*dict(nx.degree(self.G)).items())
-        for i in self.G.nodes:
-            for j in range(len(self.G.nodes)):
-                for case_val in range(self.cases):
-                    x_i, x_j = sympy.symbols(f"x{i}"), sympy.symbols(f"x{j}")
-                    if self.one_hot_encoding:
-                        if i >= j:
-                            continue
-                        s_i = str(self._encode_discrete_to_one_hot(x_i, case_val))
-                        s_j = str(self._encode_discrete_to_one_hot(x_j, case_val))
-                        if s_i == s_j:
-                            continue
-                        equation[(s_i, s_j)] = self.B[i, j]
-                    else:
-                        x_i, x_j = str(x_i), str(x_j)
-                        equation[(x_i, x_j)] = self.B[i, j]
-                        if i == j:
-                            equation[(x_i, x_j)] -= ((1 - self.resolution) * 
-                                                      np.array(degrees, dtype=np.float32)[i])
+        for i in range(len(self.B)):
+            for j in range(len(self.B)):
+                if len(self.B) != self.G.number_of_nodes():
+                    c_i, c_j = f"x{self.community[i]}", f"x{self.community[j]}"
+                    equation[(c_i, c_j)] = self.B[i, j]
+                    if i == j:
+                        equation[(c_i, c_j)] -= ((1 - self.resolution) * 
+                                        np.array(degrees, dtype=np.float32)[i])
+                else:
+                    for case_val in range(self.cases):
+                        x_i, x_j = sympy.symbols(f"x{self.community[i]}"), sympy.symbols(f"x{self.community[j]}")
+                        # TODO
+                        if self.one_hot_encoding:
+                            if i >= j:
+                                continue
+                            s_i = str(self._encode_discrete_to_one_hot(x_i, case_val))
+                            s_j = str(self._encode_discrete_to_one_hot(x_j, case_val))
+                            if s_i == s_j:
+                                continue
+                            equation[(s_i, s_j)] = self.B[i, j]
+                            equation[(s_j, s_i)] = self.B[j, i]
+                        else:
+                            x_i, x_j = str(x_i), str(x_j)
+                            equation[(x_i, x_j)] = self.B[i, j]
+                            if i == j:
+                                equation[(x_i, x_j)] -= ((1 - self.resolution) * 
+                                                            np.array(degrees, dtype=np.float32)[i])
+                                continue
+                            equation[(x_j, x_i)] = self.B[j, i]
 
         equation = {key: -1 * val for key, val in equation.items()}
-        
+
         self.objective_function = Expression(equation)
 
     def _set_one_hot_constraints(self, communities: int) -> None:
