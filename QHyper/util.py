6e0c76851626f791677d2086e1f59c8efc25b36d
tmek1244 <tmek1244@wp.pl>
2024-04-24 22:45:35 +0200

    Search for user's modules

--- QHyper/util.py
+++ QHyper/util.py
@@ -2,6 +2,11 @@
 # Smart Growth Operational Programme (2014-2020), Measure 4.2
 # under the grant agreement no. POIR.04.02.00-00-D014/20-00
 
+import importlib
+import pathlib
+import os
+import inspect
+
 import numpy.typing as npt
 
 from typing import Callable, NewType
@@ -37,10 +42,9 @@ def sort_solver_results(
 ) -> dict[str, float]:
     limit_results = limit_results or len(results)
     return {
-        k: v
-        for k, v in sorted(results.items(), key=lambda item: item[1], reverse=True)[
-            :limit_results
-        ]
+        k: v for k, v
+        in sorted(results.items(), key=lambda item: item[1],
+                  reverse=True)[:limit_results]
     }
 
 
@@ -65,3 +69,40 @@ def add_evaluation_to_results(
     """
 
     return {k: (v, score_function(k, penalty)) for k, v in results.items()}
+
+
+def search_for(class_type: type, path: str,  depth: int = 0) -> list[type]:
+    cwd = os.getcwd()
+
+    _path = pathlib.Path(path)
+    print(f"Searching for {class_type} in {_path}")
+    solvers = []
+    if _path.is_file():
+        if _path.name.endswith('.py') and _path.name != '__init__.py':
+            module_name = _path.name[:-3]  # Remove the ".py" extension
+            module_path = os.path.join(cwd, _path)
+            # Try to import the module
+            try:
+                spec = importlib.util.spec_from_file_location(
+                    module_name, module_path)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+
+                for name, obj in inspect.getmembers(module):
+                    if (
+                        inspect.isclass(obj)
+                        and issubclass(obj, class_type)
+                        and obj != class_type
+                    ):
+                        solvers.append(obj)
+                        print(f"Found solver: {obj}")
+
+            except Exception as e:
+                print(f"Failed to import {module_name}: {e}")
+
+            # loc.append(str(_path.resolve()))  # append to list if match
+    elif _path.is_dir() and depth < 2:  # check depth
+        for item in _path.iterdir():   # iterate directory contents
+            print(f"Searching in {item}")
+            solvers += search_for(type, str(item), depth + 1)
+    return solvers
